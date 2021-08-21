92a844d707a65b0e8e20185d55e7927c3e9c72af
Mekhala Vithala <info@playgroundengineer.com>
2021-08-21 08:06:49 -0500

    update creative coders

---
+++ CreativeCoder/CreativeCoder.playgroundbook/Contents/Chapters/ShapesAndSprites.playgroundchapter/Pages/Shape.playgroundpage/main.swift
@@ -0,0 +1,33 @@
+//#-hidden-code
+//
+//  See LICENSE folder for this template’s licensing information.
+//
+//  Abstract:
+//  The Swift file containing the source code edited by the user of this playground book.
+//
+//#-end-hidden-code
+
+import SpriteKit
+
+setScene()
+
+showGrid()
+
+let circle = ShapeNode(circleOfRadius: 50.0)
+canvas.addChild(circle)
+circle.fillColor = #colorLiteral(red: 0.9994556308, green: 0.9863594174, blue: 0.2532016337, alpha: 1.0)
+circle.lineWidth = 2
+circle.zPosition = 1
+circle.glowWidth = 10
+circle.position = CGPoint(x: 0, y: 0)
+
+
+let rect = ShapeNode(rect: CGRect(x: -170, y: -200, width: 100, height: 200))
+rect.fillColor = #colorLiteral(red: 0.4791436195, green: 0.2884480059, blue: 0.0, alpha: 1.0)
+rect.zPosition = 1
+canvas.addChild(rect)
+
+let ellipse = ShapeNode(ellipseIn: CGRect(x: -170, y: -50, width: 100, height: 75))
+ellipse.fillColor = #colorLiteral(red: 0.9443791509, green: 0.7896900177, blue: 0.9972682595, alpha: 1.0)
+ellipse.zPosition = 1
+canvas.addChild(ellipse)
