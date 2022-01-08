594b16b21e5664909941483f36d40a31cfdf8c95
Playground <info@playgroundengineer.com>
2022-01-08 15:57:07 -0600

    update creative coder playground to have code completion

--- CreativeCoder/CreativeCoder.playgroundbook/Contents/Chapters/Projects.playgroundchapter/Pages/Project-3.playgroundpage/main.swift
+++ CreativeCoder/CreativeCoder.playgroundbook/Contents/Chapters/Projects.playgroundchapter/Pages/Project-3.playgroundpage/main.swift
@@ -1,10 +1,8 @@
 //#-hidden-code
 //
-//  See LICENSE folder for this template’s licensing information.
+//  Copyright © 2016-2022 Satyananda Corp. All rights reserved.
 //
-//  Abstract:
-//  The Swift file containing the source code edited by the user of this playground book.
-//
+//#-code-completion(everything, show)
 //#-end-hidden-code
 
 import SpriteKit
@@ -12,3 +10,30 @@ import SpriteKit
 setScene()
 
 showGrid()
+
+//Challenge: Create checkerboard pattern
+
+//size represents size of each square. modify it as you like
+let size = 50
+
+//row function helps to create one row in checkerboard
+func row(x: Int, y: Int, first: UIColor, second: UIColor) {
+    var x = x
+    for i in 1 ... 4 {
+        let firstSquare = ShapeNode(rectOf: CGSize(width: size, height: size))
+        firstSquare.color = first
+        firstSquare.position.x = CGFloat(x)
+        firstSquare.position.y = CGFloat(y)
+        canvas.addChild(firstSquare)
+        x += size * 2
+        
+        let secondSquare = ShapeNode(rectOf: CGSize(width: size, height: size))
+        secondSquare.color = second
+        canvas.addChild(secondSquare)
+        secondSquare.position.x = firstSquare.position.x + CGFloat(size)
+        secondSquare.position.y = CGFloat(y)
+    }
+}
+
+//use row Function and for loop to create checkerboard pattern
+row(x: 0, y: 0, first: .black, second: .white)
--- CreativeCoder/CreativeCoder.playgroundbook/Contents/Modules/BookAPI.playgroundmodule/Sources/GameScene.swift
+++ CreativeCoder/CreativeCoder.playgroundbook/Contents/Modules/BookAPI.playgroundmodule/Sources/GameScene.swift
@@ -15,56 +15,53 @@ public var onContactEnd: (_ contact: SKPhysicsContact) -> Void = {contact in }
 public var onActionComplete = {}
 public let cameraNode = SKCameraNode()
 
-open class GameScene: SKScene, SKPhysicsContactDelegate, SKSceneDelegate {
+class GameScene: SKScene, SKPhysicsContactDelegate, SKSceneDelegate {
     
-    public override func didMove(to view: SKView) {
+    override func didMove(to view: SKView) {
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         self.physicsWorld.contactDelegate = self
         self.delegate = self
         self.camera = cameraNode
-        let size = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height-(UIScreen.main.bounds.height/10))
-        canvas = ShapeNode(rectOf: size)
+        canvas = ShapeNode(rectOf: self.frame.size)
         canvas.zPosition = -1
         canvas.fillColor = .systemPink
         self.addChild(canvas)
     }
     
-    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touchedNodes = nodes(at: touches.first?.location(in: self) ?? .zero)
         for node in touchedNodes {
             node.touchesBegan(touches, with: event)
         }
     }
     
-    public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touchedNodes = nodes(at: touches.first?.location(in: self) ?? .zero)
         for node in touchedNodes {
             node.touchesMoved(touches, with: event)
         }
     }
     
-    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touchedNodes = nodes(at: touches.first?.location(in: self) ?? .zero)
         for node in touchedNodes {
             node.touchesEnded(touches, with: event)
         }
     }
     
-    public override func update(_ currentTime: TimeInterval) {
-        //removeOutOfScreenNodes()
+    override func update(_ currentTime: TimeInterval) {
         onUpdate(currentTime)
     }
     
-    public func didBegin(_ contact: SKPhysicsContact) {
+    func didBegin(_ contact: SKPhysicsContact) {
         OnContactBegin(contact)
     }
     
-    public func didEnd(_ contact: SKPhysicsContact) {
+    func didEnd(_ contact: SKPhysicsContact) {
         onContactEnd(contact)
     }
     
-    public override func didEvaluateActions() {
+    override func didEvaluateActions() {
         onActionComplete()
     }
 }
-
